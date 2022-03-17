cca4c21611c8da6d7a260e02c87ebf59b6aa6d8e
dantorval-us <dantorval@alum.us.es>
2022-03-17 22:52:37 +0100

    Añade entidad Quantity

---
+++ src/main/java/acme/entities/quantity/Quantity.java
@@ -0,0 +1,41 @@
+package acme.entities.quantity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import acme.entities.ToolKit;
+import acme.entities.item.Item;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Quantity extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+	
+	// Attributes -------------------------------------------------------------
+	
+	@Min(0)
+	protected Integer quantity;
+	
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	protected Item item;
+	
+	@NotNull
+	@Valid
+	@ManyToOne
+	protected ToolKit toolkit;
+	
+}
