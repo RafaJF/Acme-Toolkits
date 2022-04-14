c8217e28f063ca9396bc2378656123511b47939a
JesusRG <rivero.gallardo.jesus@gmail.com>
2022-04-14 17:43:37 +0200

    Task 06: Done

--- src/main/java/acme/entities/announcement/AnnouncementRespository.java
+++ src/main/java/acme/entities/announcement/AnnouncementRespository.java
@@ -1,9 +1,19 @@
 package acme.entities.announcement;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Collection;
+import java.util.Date;
 
-public interface AnnouncementRespository extends CrudRepository<Announcement, Integer> {
-	
-	
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnnouncementRespository extends AbstractRepository {
+	
+	@Query("SELECT a FROM Announcement a WHERE a.creationMoment > :deadline")
+	Collection<Announcement> findAllRecentAnnouncements(Date deadline);
+	
+	@Query("SELECT a FROM Announcement a WHERE a.id = :id")
+	Announcement findAnnouncementById(int id);
 }
---
+++ src/main/java/acme/entities/announcement/AnnouncementShowService.java
@@ -0,0 +1,39 @@
+package acme.entities.announcement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Authenticated;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AnnouncementShowService implements AbstractShowService<Authenticated, Announcement>  {
+	
+	@Autowired
+	protected AnnouncementRespository announcementRespository;
+
+	@Override
+	public boolean authorise(final Request<Announcement> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public Announcement findOne(final Request<Announcement> request) {
+		assert request != null;
+		final int announcementId = request.getModel().getInteger("id");
+		final Announcement result = this.announcementRespository.findAnnouncementById(announcementId);
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Announcement> request, final Announcement entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "creationMoment","title","body","criticalFlag","info");
+	}
+}
