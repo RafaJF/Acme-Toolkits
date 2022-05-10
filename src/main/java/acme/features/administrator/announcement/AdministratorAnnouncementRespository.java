debdd7e8ea2aa677e926b96c0708e85e2d365453
JesusRG <rivero.gallardo.jesus@gmail.com>
2022-05-10 12:49:17 +0200

    Task 09D4: Done

---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementRespository.java
@@ -0,0 +1,11 @@
+package acme.features.administrator.announcement;
+
+
+import org.springframework.stereotype.Repository;
+
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AdministratorAnnouncementRespository extends AbstractRepository {
+
+}
