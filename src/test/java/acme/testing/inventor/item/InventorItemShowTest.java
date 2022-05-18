e4401f7bab122d0a98aa524e157313b65b267b96
Rafa Jiménez <rafaelangeljimenezfer@gmail.com>
2022-05-18 17:42:21 +0200

    inventorItemTest FIXED
    TESTS WORKING.

--- src/test/java/acme/testing/inventor/item/InventorItemShowTest.java
+++ src/test/java/acme/testing/inventor/item/InventorItemShowTest.java
@@ -9,13 +9,39 @@ import acme.testing.TestHarness;
 public class InventorItemShowTest extends TestHarness{
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/inventor/item/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/inventor/item/showTools.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveToolsTest(final int recordIndex,final String name, final String code,final String technology, final String description, final String retailPrice,
 		final String info) {
 			
 		super.signIn("inventor1", "inventor1");
 		super.clickOnMenu("Inventor", "List my tools");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		
+		super.checkColumnHasValue(recordIndex, 0, name);
+		super.checkColumnHasValue(recordIndex, 1, code);
+		super.checkColumnHasValue(recordIndex, 2, retailPrice);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("info", info);
+		
+		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/item/showComponents.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveComponentTest(final int recordIndex,final String name, final String code,final String technology, final String description, final String retailPrice,
+		final String info) {
+			
+		super.signIn("inventor1", "inventor1");
+		super.clickOnMenu("Inventor", "List my components");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
