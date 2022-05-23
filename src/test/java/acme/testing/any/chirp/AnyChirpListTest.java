abf5d1613a7fb75786469386c361b1e0e1298227
JesusRG <rivero.gallardo.jesus@gmail.com>
2022-05-23 13:43:36 +0200

    TestDataConverter: Done

--- src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
+++ src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
@@ -4,23 +4,26 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import acme.testing.TestHarness;
+import acme.testing.TemporalAwareTestHarness;
 
-public class AnyChirpListTest extends TestHarness{
+public class AnyChirpListTest extends TemporalAwareTestHarness{
 	
 	//Test cases ----------------------------------
 	
 	@ParameterizedTest
 	@CsvFileSource(resources="/any/chirp/list-recent-chirps.csv", encoding = "utf-8", numLinesToSkip=1)
 	@Order(10)
-	public void positiveToolTest(final int recordIndex, final String creationMoment, final String title, final String autor, final String body,
+	public void positiveToolTest(final int recordIndex, final int deltaDays, final String title, final String autor, final String body,
 		final String email) {
 	
+		String moment;
+		moment = super.computeDeltaMoment(deltaDays);
+		
 		super.clickOnMenu("Anonymous", "Chirp list");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
 		super.checkColumnHasValue(recordIndex, 1, title);
 		super.checkColumnHasValue(recordIndex, 2, autor);
 		super.checkColumnHasValue(recordIndex, 3, body);
